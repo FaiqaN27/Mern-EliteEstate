dc45b76217377144b46a5103d8183805de73351a
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-10-04 12:36:09 +0500

    create sign in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { handleError } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const handleUserSignup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -19,4 +21,33 @@ export const handleUserSignup = async (req, res, next) => {
   catch (error) {
     next(error);
   }
+}
+
+export const handleUserSignin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(handleError(404, 'User not found'));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(handleError(401, 'Invalid credentials'));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    res
+      .cookie('token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  }
+  catch (error) {
+    next(error);
+  }
+
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { handleUserSignup } from "../controllers/auth.controller.js";
+import { handleUserSignup, handleUserSignin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post('/signup', handleUserSignup);
+router.post('/signin', handleUserSignin)
 
 export default router;
