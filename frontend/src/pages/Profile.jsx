8bbfdbfbfcf4c0f631cdcdc3ce6a21c8fba49158
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-10-10 17:30:15 +0500

    complete update user functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,44 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice';
 
 const Profile = () => {
 
-  const { currentUser } = useSelector(state => state.user);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const { loading, error, currentUser } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setUpdateSuccess(false);
+  }
 
   const handleUpdate = async (e) => {
     e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    }
+    catch (err) {
+      dispatch(updateUserFailure(err.message));
+    }
   }
   return (
     <div className='p-3 max-w-lg mx-auto mb-12'>
@@ -15,13 +48,13 @@ const Profile = () => {
       <form onSubmit={handleUpdate} className='flex flex-col gap-5'>
         <img src={currentUser.avatar} className='rounded-full w-25 h-25 object-cover cursor-pointer self-center mt-2' alt='profile' />
 
-        <input type='text' className='rounded-lg p-3 bg-white focus:outline-none' placeholder='username' id='username' defaultValue={currentUser.username} />
+        <input type='text' className='rounded-lg p-3 bg-white focus:outline-none' placeholder='username' id='username' defaultValue={currentUser.username} onChange={handleChange} />
 
-        <input type='email' className='rounded-lg p-3 bg-white focus:outline-none' placeholder='email' id='email' defaultValue={currentUser.email} default={currentUser.email} />
+        <input type='email' className='rounded-lg p-3 bg-white focus:outline-none' placeholder='email' id='email' defaultValue={currentUser.email} onChange={handleChange} />
 
-        <input type='password' className='rounded-lg p-3 bg-white focus:outline-none' id='password' placeholder='password' />
+        <input type='password' className='rounded-lg p-3 bg-white focus:outline-none' id='password' placeholder='password' onChange={handleChange} />
 
-        <button className='bg-slate-700 p-3 rounded-lg uppercase text-white cursor-pointer hover:opacity-95 disabled:opacity-80'>Update</button>
+        <button disabled={loading} className='bg-slate-700 p-3 rounded-lg uppercase text-white cursor-pointer hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading ...' : 'Update'}</button>
       </form>
 
       {/* <button className='bg-green-700 p-3 rounded-lg uppercase text-white cursor-pointer hover:opacity-95 disabled:opacity-80'>Create Listing</button> */}
@@ -30,6 +63,9 @@ const Profile = () => {
         <span className='text-red-700 hover:underline cursor-pointer'>Delete Account</span>
         <span className='text-red-700 hover:underline cursor-pointer'>Sign out</span>
       </div>
+
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess ? 'User Is Updated Successfully!' : ''}</p>
     </div>
   )
 }
