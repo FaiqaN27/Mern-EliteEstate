993041daf0baebcb2d6b82b8373dad153c241ab7
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-10-21 18:42:56 +0500

    create search api route

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { handleGetListing, handleCreateListing, handleDeleteListing, handleUpdateListing } from "../controllers/listing.controller.js";
+import { handleGetListing, handleCreateListing, handleDeleteListing, handleUpdateListing, handleGetSearchListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { upload } from '../middlewares/multer.cloudinary.js';
 import { handleDeleteImage, handleUploadImage } from "../controllers/cloudinary.controller.js";
@@ -10,6 +10,7 @@ router.get('/get/:id', handleGetListing);
 router.post('/create', verifyToken, handleCreateListing);
 router.delete('/delete/:id', verifyToken, handleDeleteListing)
 router.put('/update/:id', verifyToken, handleUpdateListing);
+router.get('/get', handleGetSearchListings);
 
 //for cloudinary 
 router.post('/uploadImg', verifyToken, upload.array('images', 6), handleUploadImage);
