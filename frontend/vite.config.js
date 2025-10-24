4fbd7d3dd28cc3256a3e95dd91ca61272cfc3408
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-10-24 23:01:10 +0500

    add tailwind config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,28 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import lineClamp from '@tailwindcss/line-clamp'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api':
-      {
+      '/api': {
         target: 'http://localhost:3000',
         secure: false,
-      }
-    }
+      },
+    },
   },
   plugins: [
-    tailwindcss({
-      config: {
-        theme: {
-          extend: {},
-        },
-        plugins: [lineClamp()], // 👈 add it here
-      },
-    }),
+    tailwindcss(),
     react(),
   ],
 })
