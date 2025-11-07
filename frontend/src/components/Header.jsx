952ba83793bdfa7bca3563a96b5cb563e337bff0
FaiqaN27 <faiqanadeem6725@gmail.com>
2025-11-07 20:38:43 +0500

    change header ui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -31,32 +31,32 @@ const Header = () => {
   }, [location.search]);
 
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header>
       <div className="flex items-center justify-between max-w-6xl mx-auto p-3">
         <Link to='/'>
           <h1 className="font-bold text-sm sm:text-3xl flex flex-wrap">
-            <span className="text-slate-500">Elite</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-primary">Elite</span>
+            <span className="text-grayish">Estate</span>
           </h1>
         </Link>
 
-        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
-          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64"
+        <form onSubmit={handleSubmit} className="bg-background p-3 rounded-lg flex items-center">
+          <input type="text" placeholder="Search..." className="text-secondary focus:outline-none w-24 sm:w-64"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)} />
           <button>
-            <FaSearch className='text-slate-600' />
+            <FaSearch className='text-primary font-bold cursor-pointer' />
           </button>
         </form>
 
         <ul className='flex gap-4 text-[16px] items-center'>
 
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+            <li className='hidden sm:inline text-primary font-semibold hover:underline'>Home</li>
           </Link>
 
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+            <li className='hidden sm:inline text-secondary font-semibold  hover:underline hover:text-primary'>About</li>
           </Link>
 
           <Link to='/profile'>
