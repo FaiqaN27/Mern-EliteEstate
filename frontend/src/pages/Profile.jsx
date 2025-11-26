63dfbe7606fc518adb5d7959a5117641e2b817c7
FaiqaN27 <faiqanadeem6725@gmail.com>
2025-11-27 00:21:24 +0500

    resolve an issue

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,31 +1,40 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
 
-  const { loading, error, currentUser } = useSelector(state => state.user);
+  const { loading, error, currentUser } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
     setUpdateSuccess(false);
-  }
+  };
 
   const handleUpdate = async (e) => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-type': 'application/json',
+          "Content-type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
 
       const data = await res.json();
@@ -36,17 +45,16 @@ const Profile = () => {
 
       dispatch(updateUserSuccess(data));
       setUpdateSuccess(true);
-    }
-    catch (err) {
+    } catch (err) {
       dispatch(updateUserFailure(err.message));
     }
-  }
+  };
 
   const handleDeleteUser = async () => {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-        method: 'DELETE'
+        method: "DELETE",
       });
       const data = await res.json();
       if (data.success === false) {
@@ -54,65 +62,108 @@ const Profile = () => {
         return;
       }
       dispatch(deleteUserSuccess(data));
-
-    }
-    catch (error) {
+    } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
-  }
+  };
 
   const handleUserSignout = async () => {
     try {
       dispatch(signOutUserStart());
-      const res = await fetch('/api/auth/signout');
+      const res = await fetch("/api/auth/signout");
       const data = await res.json();
       if (data.success == false) {
         dispatch(signOutUserFailure(data.message));
         return;
       }
       dispatch(signOutUserSuccess(data));
-    }
-    catch (err) {
+    } catch (err) {
       dispatch(signOutUserFailure(error.message));
     }
-  }
+  };
 
   return (
-    <div className='p-3 max-w-lg mx-auto mb-12'>
+    <div className="p-3 max-w-lg mx-auto mb-12">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form onSubmit={handleUpdate} className="flex flex-col gap-5">
+        <img
+          src={currentUser?.avatar}
+          className="rounded-full w-25 h-25 object-cover cursor-pointer self-center mt-2"
+          alt="profile"
+        />
 
-      <form onSubmit={handleUpdate} className='flex flex-col gap-5'>
+        <input
+          type="text"
+          className="rounded-lg p-3 bg-background focus:outline-none"
+          placeholder="username"
+          id="username"
+          defaultValue={currentUser.username}
+          onChange={handleChange}
+        />
 
-        <img src={currentUser?.avatar} className='rounded-full w-25 h-25 object-cover cursor-pointer self-center mt-2' alt='profile' />
+        <input
+          type="email"
+          className=" rounded-lg p-3 bg-background focus:outline-none"
+          placeholder="email"
+          id="email"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
+        />
 
-        <input type='text' className='rounded-lg p-3 bg-background focus:outline-none' placeholder='username' id='username' defaultValue={currentUser.username} onChange={handleChange} />
+        <input
+          type="password"
+          className="rounded-lg p-3 bg-background focus:outline-none"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
 
-        <input type='email' className=' rounded-lg p-3 bg-background focus:outline-none' placeholder='email' id='email' defaultValue={currentUser.email} onChange={handleChange} />
+        <button
+          disabled={loading}
+          className="bg-primary p-3 rounded-lg uppercase text-white font-semibold cursor-pointer hover:opacity-90 disabled:opacity-80"
+        >
+          {loading ? "Loading ..." : "Update"}
+        </button>
 
-        <input type='password' className='rounded-lg p-3 bg-background focus:outline-none' id='password' placeholder='password' onChange={handleChange} />
-
-        <button disabled={loading} className='bg-primary p-3 rounded-lg uppercase text-white font-semibold cursor-pointer hover:opacity-90 disabled:opacity-80'>{loading ? 'Loading ...' : 'Update'}</button>
-
-        <Link className='bg-action p-3 rounded-lg uppercase font-semibold text-center text-white cursor-pointer hover:opacity-90' to={'/create-listing'}> Create Listing</Link>
+        <Link
+          className="bg-action p-3 rounded-lg uppercase font-semibold text-center text-white cursor-pointer hover:opacity-90"
+          to={"/create-listing"}
+        >
+          {" "}
+          Create Listing
+        </Link>
       </form>
 
-      <p className='text-danger mt-5'>{error ? error : ''}</p>
-      <p className='text-action mt-5'>{updateSuccess ? 'User Is Updated Successfully!' : ''}</p>
+      <p className="text-danger mt-5">{error ? error.message : ""}</p>
+      <p className="text-action mt-5">
+        {updateSuccess ? "User Is Updated Successfully!" : ""}
+      </p>
 
-      <div className='flex justify-between mt-5'>
-        <span className='text-danger hover:underline cursor-pointer' onClick={handleDeleteUser}>Delete Account</span>
-        <span className='text-danger hover:underline cursor-pointer' onClick={handleUserSignout}>Sign out</span>
+      <div className="flex justify-between mt-5">
+        <span
+          className="text-danger hover:underline cursor-pointer"
+          onClick={handleDeleteUser}
+        >
+          Delete Account
+        </span>
+        <span
+          className="text-danger hover:underline cursor-pointer"
+          onClick={handleUserSignout}
+        >
+          Sign out
+        </span>
       </div>
 
-      <div className='flex justify-center mt-5'>
-        <Link to={'/show-listings'}>
-          <span className='text-action hover:underline font-semibold mt-5'>Show Listings</span>
+      <div className="flex justify-center mt-5">
+        <Link to={"/show-listings"}>
+          <span className="text-action hover:underline font-semibold mt-5">
+            Show Listings
+          </span>
         </Link>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux';
-import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInFailure,
+  signInSuccess,
+} from "../redux/user/userSlice";
 import OAuth from "../components/OAuth";
 
 const SignIn = () => {
-
   const [formData, setFormData] = useState({});
 
   const navigate = useNavigate();
@@ -14,54 +17,74 @@ const SignIn = () => {
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-type': 'application/json'
+          "Content-type": "application/json",
         },
-        body: JSON.stringify(formData)
-      })
+        body: JSON.stringify(formData),
+      });
       const data = await res.json();
 
       if (data.success === false) {
-        dispatch(signInFailure(data.message))
+        dispatch(signInFailure(data.message));
         return;
       }
       dispatch(signInSuccess(data));
-      navigate('/');
+      navigate("/");
+    } catch (err) {
+      dispatch(signInFailure(err.message));
     }
-    catch (err) {
-      dispatch(signInFailure(error.message))
-    }
-  }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto ">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
 
       <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-        <input type="email" placeholder="Email" id="email" className="bg-background rounded-lg p-3 focus:outline-none" onChange={handleChange} />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-background rounded-lg p-3 focus:outline-none"
+          onChange={handleChange}
+        />
 
-        <input type="password" placeholder="Password" id="password" className="bg-background p-3 rounded-lg focus:outline-none" onChange={handleChange} />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-background p-3 rounded-lg focus:outline-none"
+          onChange={handleChange}
+        />
 
-        <button disabled={loading} className="bg-primary font-semibold p-3 rounded-lg text-white uppercase cursor-pointer hover:opacity-90 disabled:opacity-80">{loading ? 'Loading...' : 'Sign In'}</button>
+        <button
+          disabled={loading}
+          className="bg-primary font-semibold p-3 rounded-lg text-white uppercase cursor-pointer hover:opacity-90 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
         <OAuth />
       </form>
 
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
-        <Link to={'/sign-up'}><span className="text-primary font-semibold hover:underline">Sign up</span></Link>
+        <Link to={"/sign-up"}>
+          <span className="text-primary font-semibold hover:underline">
+            Sign up
+          </span>
+        </Link>
       </div>
 
-      {error && <p className="text-danger mt-5">{error}</p>}
+      {error && <p className="text-danger mt-5">{error.message}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
