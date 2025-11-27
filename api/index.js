3730634365cd0c0eb92ba98cdaa2f3a6a3e0f6d4
FaiqaN27 <faiqanadeem6725@gmail.com>
2025-11-27 09:32:07 +0500

    structure Db folder

--- api/index.js
+++ api/index.js
@@ -1,22 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import connectDB from "./db/connectDB.js";
 dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log('Connected to MongoDB!')
-}).catch((err) => {
-  console.log(err);
-})
-
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
@@ -25,15 +32,14 @@ app.use("/api/listing", listingRouter);
 //error middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message
+    message,
   });
-})
-
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on PORT 3000');
-})
+  console.log("Server is running on PORT 3000");
+});
