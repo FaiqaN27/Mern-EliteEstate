c52aa3f280aaf4d6e2de9dd45115f34c9408dcce
FaiqaN27 <faiqanadeem6725@gmail.com>
2025-11-27 09:35:53 +0500

    resolve db error

--- api/db/connectDB.js
+++ api/db/connectDB.js
@@ -2,7 +2,7 @@ import { mongoose } from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB Connected");
   } catch (err) {
     console.log("Database Connection Error", err);
